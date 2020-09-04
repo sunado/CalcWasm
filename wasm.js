01e6fb15b07a8b291b93d30cfb08ab62c61677a6
sunado <quangdv1996@gmail.com>
2020-09-04 08:30:31 +0000

    update ui

--- wasm.js
+++ wasm.js
@@ -8,6 +8,7 @@ async function run (){
             var expression = $("#tb-input").val();
             var number = calculator(expression, "");
             $("#tb-result").append(">>" + expression + " = " + number + "<br />");
+            $('#div-result').scrollTop($('#div-result')[0].scrollHeight);
         })
         
         $("#btn-hex2dec").click( () => {
@@ -16,12 +17,14 @@ async function run (){
             //console.log("GET:" + numberHex);
             var number = hex_to_dec(numberHex);
             $("#tb-result-hex").append(">>" + numberHex + " = " + number + "<br />");
+            $('#div-result-hex').scrollTop($('#div-result-hex')[0].scrollHeight);
         })
 
         $("#btn-dec2hex").click( () => {
             var numberDecString = $("#tb-input-dec").val();
             var number = dec_to_hex(numberDecString);
             $("#tb-result-dec").append(">>" + numberDecString + " = " + number + "<br />");
+            $('#div-result-dec').scrollTop($('#div-result-dec')[0].scrollHeight);
         })
 
         $("#btn-bin2dec").click( () => {
@@ -30,12 +33,14 @@ async function run (){
             //console.log("GET:" + numberHex);
             var number = bin_to_dec(numberHex);
             $("#tb-result-bin2dec").append(">>" + numberHex + " = " + number + "<br />");
+            $('#div-result-bin2dec').scrollTop($('#div-result-bin2dec')[0].scrollHeight);
         })
 
         $("#btn-dec2bin").click( () => {
             var numberDecString = $("#tb-input-dec2bin").val();
             var number = dec_to_bin(numberDecString);
             $("#tb-result-dec2bin").append(">>" + numberDecString + " = " + number + "<br />");
+            $('#div-result-dec2bin').scrollTop($('#div-result-dec2bin')[0].scrollHeight);
         })
 
     });
